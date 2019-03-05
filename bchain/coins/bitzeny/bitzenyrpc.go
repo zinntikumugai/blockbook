8f9e99d4615e1b05bcc9873ad5ccf074d544b1ec
zinntikumugai <uesitananame55@gmail.com>
2019-03-05 15:39:52 +0900

    add bitzeny

---
+++ bchain/coins/bitzeny/bitzenyrpc.go
@@ -0,0 +1,57 @@
+package bitzeny
+
+import (
+	"blockbook/bchain"
+	"blockbook/bchain/coins/btc"
+	"encoding/json"
+
+	"github.com/golang/glog"
+)
+
+// BitZenyRPC is an interface to JSON-RPC bitcoind service.
+type BitZenyRPC struct {
+	*btc.BitcoinRPC
+}
+
+// NewBitZenyRPC returns new BitZenyRPC instance.
+func NewBitZenyRPC(config json.RawMessage, pushHandler func(bchain.NotificationType)) (bchain.BlockChain, error) {
+	b, err := btc.NewBitcoinRPC(config, pushHandler)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &BitZenyRPC{
+		b.(*btc.BitcoinRPC),
+	}
+	s.RPCMarshaler = btc.JSONMarshalerV2{}
+	s.ChainConfig.SupportsEstimateFee = false
+
+	return s, nil
+}
+
+// Initialize initializes BitZenyRPC instance.
+func (b *BitZenyRPC) Initialize() error {
+	chainName, err := b.GetChainInfoAndInitializeMempool(b)
+	if err != nil {
+		return err
+	}
+
+	glog.Info("Chain name ", chainName)
+	params := GetChainParams(chainName)
+
+	// always create parser
+	b.Parser = NewBitZenyParser(params, b.ChainConfig)
+
+	// parameters for getInfo request
+	if params.Net == MainnetMagic {
+		b.Testnet = false
+		b.Network = "livenet"
+	} else {
+		b.Testnet = true
+		b.Network = "testnet"
+	}
+
+	glog.Info("rpc: block chain ", params.Name)
+
+	return nil
+}
